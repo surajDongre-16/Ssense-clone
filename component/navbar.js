078c26484ca17ddd1c1db5f03b18cc0c0f4fa0a1
surajDongre-16 <surajdongre8@gmail.com>
2022-05-03 14:38:31 +0530

    navbar structure added

---
+++ component/navbar.js
@@ -0,0 +1,19 @@
+function navbar(){
+    return ` <div id="left-nav">
+                <p>MENSWEAR</p>
+                <p>WOMENSWEAR</p>
+                <p>EVRYTHING ELSE</p>
+                <p>SEARCH</p>
+            </div>
+            <div id="middle-nav">
+                <img id="logo" src="https://res.cloudinary.com/ssenseweb/image/upload/v1471963917/web/ssense_logo_v2.svg" alt="">
+            </div>
+            <div id="right-nav">
+                <p>ENGLISH</p>
+                <p>LOGIN</p>
+                <p>WISHLIST</p>
+                <p>SHOPPING BAG (0)</p>
+            </div>`
+}
+
+export default navbar
