82acfdbe4970c5f085c1c1a976734e962221a660
sharangahirekar1 <sharangahirekar1@gmail.com>
2022-05-07 18:53:35 +0530

    recommend section

--- scripts/product.js
+++ scripts/product.js
@@ -2,3 +2,10 @@ import navbar from '../component/navbar.js';
 
 document.getElementById("navbar").innerHTML = navbar();
 
+import {show,append} from './fetch.js';
+show("mens_accessories").then((data)=>{
+    const container= document.getElementById("recommend");
+    container.innerHTML=null
+    // console.log(data)
+    append(data,container)
+})
